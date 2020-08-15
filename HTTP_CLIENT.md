e35d176f116c941e79c25e85ca990ba25c1c3f1f
KwonNamSon <kwon37xi@gmail.com>
2020-08-15 22:05:27 +0900

    httpclient POST form

--- HTTP_CLIENT.md
+++ HTTP_CLIENT.md
@@ -1,9 +1,11 @@
 # Java 11 HttpClient
 * [HttpClient kwonnam wiki](https://kwonnam.pe.kr/wiki/java/httpclient)
-* [Java 11 HttpClient Examples - Mkyong.com](https://mkyong.com/all-tutorials-on-mkyong-com/)
+* [Java 11 HttpClient Examples - Mkyong.com](https://mkyong.com/java/java-11-httpclient-examples/)
 
 ## API
 * [HttpClient.java](https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpClient.html) : immutable, reusable
 * [HttpRequest.java](https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpRequest.html) : immutable, reusable
 * [HttpResponse.java](https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpResponse.html)
 * [WebSocket.java](https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/WebSocket.html)
+
+## Examples
--- src/test/java/kr/pe/kwonnam/research/java/httpclient/HttpClientSynchronousTest.java
+++ src/test/java/kr/pe/kwonnam/research/java/httpclient/HttpClientSynchronousTest.java
@@ -9,11 +9,15 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -55,5 +59,52 @@ class HttpClientSynchronousTest {
 
         String userAgent = ctx.read("$.headers.User-Agent");
         assertThat(userAgent).isEqualTo("Java 11 HttpClient Bot");
+    }
+
+
+    @Test
+    @DisplayName("POST 요청에 Form 파라미터 설정을 한다.")
+    void postFormParameters() throws IOException, InterruptedException {
+        Map<Object, Object> data= new HashMap<>();
+
+        data.put("username", "abc");
+        data.put("password", "123");
+        data.put("custom", "한글과 English");
+        data.put("ts", System.currentTimeMillis());
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .POST(ofFormData(data))
+            .uri(URI.create("https://httpbin.org/post"))
+            .setHeader("User-Agent", "Java 11 HttpClient POST Bot")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        log.info("POST response body : {}", response.body());
+        assertThat(response.statusCode()).isEqualTo(200);
+
+        DocumentContext ctx = JsonPath.parse(response.body());
+
+        String userAgentHeader = ctx.read("$.headers.User-Agent");
+
+        assertThat(ctx.read("$.form.custom", String.class)).isEqualTo("한글과 English");
+        assertThat(ctx.read("$.form.username", String.class)).isEqualTo("abc");
+        assertThat(ctx.read("$.form.password", String.class)).isEqualTo("123");
+        assertThat(ctx.read("$.headers.User-Agent", String.class)).isEqualTo("Java 11 HttpClient POST Bot");
+    }
+
+    private HttpRequest.BodyPublisher ofFormData(Map<Object, Object> data) {
+        var builder = new StringBuilder();
+
+        for (Map.Entry<Object, Object> entry : data.entrySet()) {
+            if (builder.length() > 0) {
+                builder.append("&");
+            }
+            builder.append(URLEncoder.encode(entry.getKey().toString(), StandardCharsets.UTF_8));
+            builder.append("=");
+            builder.append(URLEncoder.encode(entry.getValue().toString(), StandardCharsets.UTF_8));
+        }
+        return HttpRequest.BodyPublishers.ofString(builder.toString());
     }
 }
