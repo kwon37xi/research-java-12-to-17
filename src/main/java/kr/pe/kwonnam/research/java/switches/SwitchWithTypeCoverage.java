53a179d939cadedf1001727d2a09677ae38b27e1
KwonNam Son <kwon37xi@gmail.com>
2022-01-23 00:58:02 +0900

    SwitchWithTypeCoverage 주석 추가

--- src/main/java/kr/pe/kwonnam/research/java/switches/SwitchWithTypeCoverage.java
+++ src/main/java/kr/pe/kwonnam/research/java/switches/SwitchWithTypeCoverage.java
@@ -1,5 +1,12 @@
 package kr.pe.kwonnam.research.java.switches;
 
+/**
+ * {@code switch} 에서 instance type 체크하기.
+ *
+ * 아래는 preview 기능으로 컴파일과 실행시 java 에 {@code --enable-preview} 옵션이 필요하다.
+ *
+ * @see <a href="https://dzone.com/articles/features-of-java-17">Java 17 for the Impatient</a>
+ */
 public class SwitchWithTypeCoverage {
     public static void main(String[] args) {
         System.out.println("ClassTwo : " + withSealedClass(new ClassTwo()));
