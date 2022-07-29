69c732cc2349d0a03fd8e2d6e1caa7b4088121d3
KwonNam Son <kwon37xi@gmail.com>
2022-07-29 16:38:16 +0900

    [Java 16] Stream.mapMulti

--- src/test/java/kr/pe/kwonnam/research/java/streams/MapMultiTest.java
+++ src/test/java/kr/pe/kwonnam/research/java/streams/MapMultiTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
